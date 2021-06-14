a09c8d34b3533c1543224eb08cb05fc7015927af
Patrick <>
2021-06-13 22:41:59 -0400

    Update characters cypress test

--- cypress/integration/dimension345n/Characters_spec.js
+++ cypress/integration/dimension345n/Characters_spec.js
@@ -4,7 +4,6 @@ describe('Characters', () => {
     cy.visit('http://localhost:3000/')
     cy.get('.burger-container').click()
     .get('[href="/characters"]').click()
-    .url().should('include', '/characters')
   })
 
   it('Should be on characters display page', () => {
@@ -59,6 +58,13 @@ describe('Characters', () => {
     .get('[href="/"]').click({force: true})
     .get('#\\31 1 [data-cy=blank-icon]')
     .should('have.css', 'opacity', '0')
+  })
+
+  it('Should unfavorite characters after navigating back home', () => {
+    cy.get('#\\31 1 [data-cy=blank-icon]').click()
+    cy.get('.burger-container').click({force: true})
+    .get('[href="/"]').click({force: true})
+    .get('#\\31 1 [data-cy=blank-icon]').click()
   })
 
 })
