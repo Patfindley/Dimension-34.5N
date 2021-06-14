0d7e1a5dd36a74b297eee85d893271928a7e801b
Patrick <>
2021-06-13 22:58:58 -0400

    Finish characters test

--- cypress/integration/dimension345n/Episodes_spec.js
+++ cypress/integration/dimension345n/Episodes_spec.js
@@ -18,6 +18,53 @@ describe('Episodes', () => {
     cy.get("[data-cy='blank-icon']")
   })
 
-  
+  it('Should activate favorite function when blank icon is clicked', () => {
+    cy.get('#\\36 > [data-cy=blank-icon]')
+    .should('have.css', 'opacity', '1').click()
+    cy.get('#\\36 > [data-cy=blank-icon]')
+    .should('have.css', 'opacity', '0')
+  })
+
+  it('Should be able UN favorite a episode', () => {
+    cy.get('#\\36 > [data-cy=blank-icon]')
+    .should('have.css', 'opacity', '1').click()
+    cy.get('#\\36 > [data-cy=blank-icon]')
+    .should('have.css', 'opacity', '0').click()
+    cy.get('#\\36 > [data-cy=blank-icon]')
+    .should('have.css', 'opacity', '1')
+  })
+
+  it("Should navigate you to characters section when drop down link is clicked", () => {
+    cy.get('.burger-container').click({force: true})
+    .get('[href="/characters"]').click({force: true})
+    .url().should('include', '/characters')
+  })
+
+  it("Should navigate you to locations section when drop down link is clicked", () => {
+    cy.get('.burger-container').click({force: true})
+    .get('[href="/locations"]').click({force: true})
+    .url().should('include', '/locations')
+  })
+
+  it("Should navigate you back to home section when drop down link is clicked", () => {
+    cy.get('.burger-container').click({force: true})
+    .get('[href="/"]').click({force: true})
+    .url().should('include', '/')
+  })
+
+  it('Should recognize a favorite when navigating home', () => {
+    cy.get('#\\36 > [data-cy=blank-icon]').click()
+    cy.get('.burger-container').click({force: true})
+    .get('[href="/"]').click({force: true})
+    .get('#\\36 > [data-cy=blank-icon]')
+    .should('have.css', 'opacity', '0')
+  })
+
+  it('Should unfavorite episode after navigating back home', () => {
+    cy.get('#\\36 > [data-cy=blank-icon]').click()
+    cy.get('.burger-container').click({force: true})
+    .get('[href="/"]').click({force: true})
+    .get('#\\36 > [data-cy=blank-icon]').click()
+  })
 
 })
