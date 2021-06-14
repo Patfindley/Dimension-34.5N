2ada12d86c378bc69db4438e8755b95cca50cd20
Patrick <>
2021-06-13 23:06:30 -0400

    finish locations test

--- cypress/integration/dimension345n/Locations_spec.js
+++ cypress/integration/dimension345n/Locations_spec.js
@@ -40,6 +40,31 @@ describe('Locations', () => {
     .url().should('include', '/characters')
   })
 
-  
+  it("Should navigate you to episodes section when drop down link is clicked", () => {
+    cy.get('.burger-container').click({force: true})
+    .get('[href="/episodes"]').click({force: true})
+    .url().should('include', '/episodes')
+  })
+
+  it("Should navigate you back to home section when drop down link is clicked", () => {
+    cy.get('.burger-container').click({force: true})
+    .get('[href="/"]').click({force: true})
+    .url().should('include', '/')
+  })
+
+  it('Should recognize a favorite when navigating home', () => {
+    cy.get('#\\36 > [data-cy=blank-icon]').click()
+    cy.get('.burger-container').click({force: true})
+    .get('[href="/"]').click({force: true})
+    .get('#\\36 > [data-cy=blank-icon]')
+    .should('have.css', 'opacity', '0')
+  })
+
+  it('Should unfavorite location after navigating back home', () => {
+    cy.get('#\\36 > [data-cy=blank-icon]').click()
+    cy.get('.burger-container').click({force: true})
+    .get('[href="/"]').click({force: true})
+    .get('#\\36 > [data-cy=blank-icon]').click()
+  })
 
 })
