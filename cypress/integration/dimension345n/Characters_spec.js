62f10d776f13b62f7f5257f720ebbe596ba8384b
Patrick <>
2021-06-13 22:37:37 -0400

    Start Characters cypress test

--- cypress/integration/dimension345n/Characters_spec.js
+++ cypress/integration/dimension345n/Characters_spec.js
@@ -7,6 +7,34 @@ describe('Characters', () => {
     .url().should('include', '/characters')
   })
 
+  it('Should be on characters display page', () => {
+    cy.url().should('include', '/characters')
+  })
+
+  it('Should display character image', () => {
+    cy.get('[src="https://rickandmortyapi.com/api/character/avatar/11.jpeg"]')
+  })
+
+  it('Should see icon to favorite a character', () => {
+    cy.get("[data-cy='blank-icon']")
+  })
+
+  it('Should activate favorite function when blank icon is clicked', () => {
+    cy.get('#\\31 1 [data-cy=blank-icon]')
+    .should('have.css', 'opacity', '1').click()
+    cy.get('#\\31 1 [data-cy=blank-icon]')
+    .should('have.css', 'opacity', '0')
+  })
+
+  it('Should be able UN favorite a character', () => {
+    cy.get('#\\31 1 [data-cy=blank-icon]')
+    .should('have.css', 'opacity', '1').click()
+    cy.get('#\\31 1 [data-cy=blank-icon]')
+    .should('have.css', 'opacity', '0').click()
+    cy.get('#\\31 1 [data-cy=blank-icon]')
+    .should('have.css', 'opacity', '1')
+  })
+
   
 
 })
