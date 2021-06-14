c8e891f0003cb15b26979c5f86e2cfc52e21d0c9
Patrick <>
2021-06-13 23:06:06 -0400

    Update locations Cypress test

--- cypress/integration/dimension345n/Locations_spec.js
+++ cypress/integration/dimension345n/Locations_spec.js
@@ -18,6 +18,28 @@ describe('Locations', () => {
     cy.get("[data-cy='blank-icon']")
   })
 
+  it('Should activate favorite function when blank icon is clicked', () => {
+    cy.get('#\\36 > [data-cy=blank-icon]')
+    .should('have.css', 'opacity', '1').click()
+    cy.get('#\\36 > [data-cy=blank-icon]')
+    .should('have.css', 'opacity', '0')
+  })
+
+  it('Should be able UN favorite a location', () => {
+    cy.get('#\\36 > [data-cy=blank-icon]')
+    .should('have.css', 'opacity', '1').click()
+    cy.get('#\\36 > [data-cy=blank-icon]')
+    .should('have.css', 'opacity', '0').click()
+    cy.get('#\\36 > [data-cy=blank-icon]')
+    .should('have.css', 'opacity', '1')
+  })
+
+  it("Should navigate you to characters section when drop down link is clicked", () => {
+    cy.get('.burger-container').click({force: true})
+    .get('[href="/characters"]').click({force: true})
+    .url().should('include', '/characters')
+  })
+
   
 
 })
